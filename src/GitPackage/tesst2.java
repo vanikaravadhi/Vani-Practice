3c0db2c08ea0dbd5bde9be485760774b5c76f5b5
vani <vani.karavadhi@gmail.com>
2019-05-18 15:42:21 +0530

    addding new files

--- src/GitPackage/tesst2.java
+++ src/GitPackage/tesst2.java
@@ -5,6 +5,7 @@ public class tesst2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("under test2 class");
+		System.out.println("this is second line");
 
 	}
 
---
+++ src/GitPackage/test4.java
@@ -0,0 +1,10 @@
+package GitPackage;
+
+public class test4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("under test4 class");
+	}
+
+}
