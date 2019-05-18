7b5d3301d7b033e98b85372fff4246bed3e361dc
vanikaravadhi <50765129+vanikaravadhi@users.noreply.github.com>
2019-05-18 15:49:31 +0530

    Update test4.java

--- src/GitPackage/test4.java
+++ src/GitPackage/test4.java
@@ -5,6 +5,11 @@ public class test4 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("under test4 class");
+		
+		int a=20;
+		int b=20;
+		int c=a+b;
+		system.out.println(c);
 	}
 
 }
