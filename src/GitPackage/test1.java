e8850dc5c4bad55d939b0ee9d543a2ebffc37f5d
vanikaravadhi <50765129+vanikaravadhi@users.noreply.github.com>
2019-05-18 15:44:14 +0530

    Update test1.java

--- src/GitPackage/test1.java
+++ src/GitPackage/test1.java
@@ -5,6 +5,9 @@ public class test1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("under test1 class");
+		int a=30;
+		int b=10;
+		int c=a+b;
 
 	}
 
